94797be5b73ccde1dcca88def08c2c7030eceadf
rofi <rfigueroa@truelogic.io>
2024-02-14 19:42:59 -0300

    additions

--- with-tamagui/src/tamagui.config.ts
+++ with-tamagui/src/tamagui.config.ts
@@ -1,12 +1,12 @@
-import { config as configBase } from '@tamagui/config'
-import { createTamagui } from 'tamagui'
+import { config as configBase } from "@tamagui/config/v3";
+import { createTamagui } from "tamagui";
 
-export const config = createTamagui(configBase)
+export const config = createTamagui(configBase);
 
-export default config
+export default config;
 
-export type Conf = typeof config
+export type Conf = typeof config;
 
-declare module 'tamagui' {
+declare module "tamagui" {
   interface TamaguiCustomConfig extends Conf {}
 }
