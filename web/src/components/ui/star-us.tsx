c80926d660109688833ca38f34d998ec925d2dab
ROFI <rodrigofigueroa.name@gmail.com>
2024-07-17 12:24:25 -0400

    x

--- web/src/components/ui/star-us.tsx
+++ web/src/components/ui/star-us.tsx
@@ -35,7 +35,6 @@ export default function StarUs() {
       <GithubIcon className="h-5 w-5" />
       <span>Star on GitHub</span>
       <svg
-        title="star"
         xmlns="http://www.w3.org/2000/svg"
         width="16"
         height="16"
@@ -47,6 +46,7 @@ export default function StarUs() {
         strokeLinejoin="round"
         className="h-4 w-4"
       >
+        <title>star</title>
         <polygon points="12 2 15 8.5 22 9.3 17 14 18.5 21 12 17.5 5.5 21 7 14 2 9.3 9 8.5 12 2" />
       </svg>
       <span className="ml-0 rounded-md bg-muted py-1 text-sm text-muted-foreground">
@@ -60,7 +60,6 @@ function GithubIcon(props) {
   return (
     <svg
       {...props}
-      title="github icon"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -71,6 +70,7 @@ function GithubIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
+      <title>github icon</title>
       <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
       <path d="M9 18c-4.51 2-5-2-7-2" />
     </svg>
@@ -81,7 +81,6 @@ function XIcon(props) {
   return (
     <svg
       {...props}
-      title="x icon"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
@@ -92,6 +91,7 @@ function XIcon(props) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
+      <title>x icon</title>
       <path d="M18 6 6 18" />
       <path d="m6 6 12 12" />
     </svg>
